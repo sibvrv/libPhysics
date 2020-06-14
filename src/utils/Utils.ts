19fcf3d235b8db23cfeda8e7f2ead8c35bfd8d91
Roman Vereshchagin <info@sibvrv.com>
2020-06-14 12:40:05 +0700

    class Utils to TypeScript

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,24 +1,21 @@
-function Utils() {
-}
-
-module.exports = Utils;
-
 /**
- * Extend an options object with default values.
- * @static
- * @method defaults
- * @param  {object} options The options object. May be falsy: in this case, a new object is created and returned.
- * @param  {object} defaults An object containing default values.
- * @return {object} The modified options object.
+ * Utils
  */
-Utils.defaults = function(options, defaults) {
-  options = options || {};
-
-  for (var key in defaults) {
-    if (!(key in options)) {
-      options[key] = defaults[key];
+export class Utils {
+  /**
+   * Extend an options object with default values.
+   * @static
+   * @method defaults
+   * @param  {object} options The options object. May be falsy: in this case, a new object is created and returned.
+   * @param  {object} defaults An object containing default values.
+   * @return {object} The modified options object.
+   */
+  static defaults(options: any = {}, defaults: any) {
+    for (const key in defaults) {
+      if (!(key in options)) {
+        options[key] = defaults[key];
+      }
     }
+    return options;
   }
-
-  return options;
-};
+}
